d4ccc96950751e1e289fe5d747cc5fab57b8dd2b
trishabilda94-cpu <trishabilda94@gmail.com>
2025-11-22 22:53:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", function () {
+    const message = document.getElementById("message");
+    message.textContent = "Have a great day! 😊";
+});
